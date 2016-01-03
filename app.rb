b6007e18b107640a87f02bf39a32c5a1760d7441
Marck <mercercountynj@gmail.com>
2016-01-02 19:41:04 -0500

    home page

--- app.rb
+++ app.rb
@@ -1,11 +1,12 @@
 #create "Gemfile" file, add "gem 'sinatra'", and run bundle install
 require 'sinatra'
+require 'sinatra/activerecord'
+set :database, "sqlite3:myblogdb.sqlite3"
+require './models'
+
 
 
 get '/' do
     erb :home
 end
 
-post '/sign-in' do
-    puts "my params are" + params.inspect
-end
