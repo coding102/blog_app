8e0a3792a8e1b0923d38037b9155284075f610e0
Marck <mercercountynj@gmail.com>
2016-01-04 12:09:46 -0500

    homepage signin form to database

--- app.rb
+++ app.rb
@@ -1,30 +1,58 @@
 #create "Gemfile" file, add "gem 'sinatra'", and run bundle install
 require 'sinatra'
 require 'sinatra/activerecord'
-
+set :database, "sqlite3:myblogdb.sqlite3"
+require './models'
 enable :sessions
 
-set :database, "sqlite3:myblogdb.sqlite3"
-
-require './models'
 
 
+
+
+#homepage url
 get '/' do
     erb :home
 end
 
 
+
+
+
+# sign in rules
+post '/sign-in' do
+    @user = User.where(username: params[:username]).first
+    if @user.password == params[:password]
+#send to personal blog page if both inputs match someone in database
+        redirect '/blog'
+    else
+#send to home page if user is not in the database
+        redirect '/'
+    end
+end
+
+
+
+
+
+#signup page if you are a new user
 get '/signup' do
     erb :signup
 end
 
 
+
+
+
+#blog page, page you are directed to if you sign in succesfully
 get '/blog' do
     erb :signedin
 end
 
 
-get '/myaccount' do
-    erb :myaccount
-end
 
+
+
+#your account profile if you need to edit it
+get '/settings' do
+    erb :myacccount
+end
